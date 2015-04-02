c6cfba426c040c1f1a23098666076fa8b73d8da8
symbol24 <phillip.conte@gmail.com>
2015-04-01 22:47:25 -0400

    arg

---
+++ tic_tac_toe/Assets/Scripts/TicTac.cs
@@ -0,0 +1,210 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TicTac : MonoBehaviour {
+
+	// TicTac
+	//
+	// This code will compute the optimal tic-tac-toe game on a 3x3 board.
+
+		public static int BOARD_SIZE = 3;
+		protected static int EMPTY = 0;
+	public int[,]         board = new int[BOARD_SIZE,BOARD_SIZE];
+	public int             i;
+	public int             j;
+	public int[]           move = new int[2];
+	public int             player;
+	public int             value;
+	public int             moves;
+
+		public void Start() {
+			
+			
+			// Inititalize board to be empty
+			for(i = 0; i < BOARD_SIZE; i = i + 1) {
+				for(j = 0; j < BOARD_SIZE; j = j + 1) {
+					board[i,j] = EMPTY;
+				}
+			}
+			
+			// Keep moving. (We finish when game is done)
+			player = 1;
+			for(moves = 0; true; moves = moves + 1) {
+				// Print board
+				for(i = 0; i < BOARD_SIZE; i = i + 1) {
+					for(j = 0; j < BOARD_SIZE; j = j + 1) {
+						if(board[i,j] == EMPTY) {
+						Debug.Log("-");
+						} else if(board[i,j] > 0) {
+						Debug.Log("X");
+						} else {
+						Debug.Log("O");
+						}
+					}
+					Debug.Log(" ");
+				}
+				
+				// Get value of board
+				value = TicTacMove(board, player, move);
+				if(move[0] < 0) {
+					if(value == 0) {
+					Debug.Log("Cat's game");
+					} else if(value > 0) {
+					Debug.Log("X won");
+					} else {
+					Debug.Log("O won");
+					}
+					return;
+				}
+				board[move[0],move[1]] = player;
+				
+				// Print information about move
+				Debug.Log("Move " + (moves + 1) + ": ");
+				if(player > 0) {
+					Debug.Log("X");
+				} else {
+					Debug.Log("O");
+				}
+				Debug.Log(" plays at (" + (move[0] + 1) + "," + (move[1] + 1) + ") [");
+				if(value == 0) {
+					Debug.Log("nobody");
+				} else if(value > 0) {
+					Debug.Log("X");
+				} else {
+					Debug.Log("O");
+				}
+				Debug.Log(" will win]");
+				
+				// Switch players for next move
+				player = -player;
+			}
+		}
+		
+		// TicTacMove
+		//
+		// Find the best move for whosemove and place it into (best_place[0], best_place[1]).
+		// Return the value of the board. If the board is a final state, then put -1 into
+		// best_place[0].
+		public static int TicTacMove(int[,] board, int whosemove, int[] best_place) {
+			int             i;
+			int             j;
+			int[]           place = new int[2];
+			int             best;
+			int             value;
+			
+			// See if anybody has won
+			value = TicTacFinal(board);
+			if(value != EMPTY) {
+				best_place[0] = -1;
+				return value;
+			}
+			
+			// Try each available move and maximize (or minimize)
+			best = -2 * whosemove; // anything will beat this
+			for(i = 0; i < BOARD_SIZE; i = i + 1) {
+				for(j = 0; j < BOARD_SIZE; j = j + 1) {
+					if(board[i,j] == EMPTY) {
+						// Try moving here, see its value, then undo move
+						board[i,j] = whosemove;
+						value = TicTacMove(board, -whosemove, place);
+						board[i,j] = EMPTY;
+						
+						// See if value is better than what we have
+						if((whosemove < 0 && value < best)
+						   || (whosemove > 0 && value > best)) {
+							best = value;
+							best_place[0] = i;
+							best_place[1] = j;
+						}
+						
+						if(best == whosemove) {
+							// Then I found a guaranteed win; take it.
+							return best;
+						}
+					}
+				}
+			}
+			
+			if(best == -2 * whosemove) {
+				// No empty spaces are on board; it's a cat's game.
+				best_place[0] = -1;
+				return 0;
+			}
+			
+			return best;
+		}
+		
+		
+		// TicTacFinal
+		//
+		// returns the code of the player that has won in the board, or EMPTY
+		// if neither has won.
+		public static int TicTacFinal(int[,] board) {
+			int             i;
+			int             j;
+			int             k;
+			
+			// See if any row is filled by a player
+			for(i = 0; i < BOARD_SIZE; i = i + 1) {
+				k = board[i,0];
+				if(k != EMPTY) {
+					for(j = 1; j < BOARD_SIZE; j = j + 1) {
+						if(board[i,j] != k) {
+							break;
+						}
+					}
+					if(j == BOARD_SIZE) {
+						// then k has filled row
+						return k;
+					}
+				}
+			}
+			
+			// See if any column is filled by a player
+			for(j = 0; j < BOARD_SIZE; j = j + 1) {
+				k = board[0,j];
+				if(k != EMPTY) {
+					for(i = 1; i < BOARD_SIZE; i = i + 1) {
+						if(board[i,j] != k) {
+							break;
+						}
+					}
+					if(i == BOARD_SIZE) {
+						// then k has filled column
+						return k;
+					}
+				}
+			}
+			
+			// See if \ diagonal is filled by a player
+			k = board[0,0];
+			if(k != EMPTY) {
+				for(i = 0; i < BOARD_SIZE; i = i + 1) {
+					if(board[i,i] != k) {
+						break;
+					}
+				}
+				if(i == BOARD_SIZE) {
+					// then k has filled \ diagonal
+					return k;
+				}
+			}
+			
+			// See if / diagonal is filled by a player
+			k = board[0,BOARD_SIZE - 1];
+			if(k != EMPTY) {
+				for(i = 0; i < BOARD_SIZE; i = i + 1) {
+					if(board[i,BOARD_SIZE - 1 - i] != k) {
+						break;
+					}
+				}
+				if(i == BOARD_SIZE) {
+					// then k has filled / diagonal
+					return k;
+				}
+			}
+			
+			return EMPTY;
+		}
+
+}
--- tic_tac_toe/Assets/Scripts/TicTacToe.cs
+++ tic_tac_toe/Assets/Scripts/TicTacToe.cs
@@ -6,9 +6,9 @@ public class TicTacToe : MonoBehaviour {
 	public string[][] m_Players = new string[][]{
 		new string[]{"Unused", "-"}, new string[]{"Player 1", "X"}, new string[]{"Player 2","O"}
 	};
-	private int unused = 0;
-	private int player1 = 1;
-	private int player2 = 2;
+	private int m_unused = 0;
+	private int m_player1 = 1;
+	private int m_player2 = 2;
 	private int m_currentPlayer = 0;
 	public int[] m_gameBoard = new int[9];
 	private int[] m_gameValues = new int[3]{0,1,2};
@@ -16,8 +16,8 @@ public class TicTacToe : MonoBehaviour {
 		new int[]{0,1,2},new int[]{3,4,5},new int[]{6,7,8},new int[]{0,3,6},new int[]{1,4,7},new int[]{2,5,8},new int[]{0,4,8},new int[]{2,4,6},
 	};
 	private Button[] m_buttons = new Button[20];
-	private int winner = 0;
 	private string m_winningCombo = "";
+	private int m_winningMove = -1;
 
 
 	// Use this for initialization
@@ -31,17 +31,10 @@ public class TicTacToe : MonoBehaviour {
 	
 	}
 
-	public void helpme(){
-
-	}
-
 	public void ButtonPressed(Button thisButton){
 		switch (thisButton.name) {
-			case "yesButton":
-				print("Yes");
-				break;
-			case "noButton":
-				print("no");
+			case "ResetButton":
+				ResetGame();
 				break;
 			default:
 				ChangeButton(thisButton);
@@ -53,16 +46,24 @@ public class TicTacToe : MonoBehaviour {
 		if (UpdateGameBoardList (btoChange.name)) {
 			Text buttonText = btoChange.GetComponentInChildren<Text> () as Text;
 			buttonText.text = m_Players [m_currentPlayer] [1];
-			if(!GameOver()) m_currentPlayer = GetNextPlayer (m_currentPlayer);
+			if(!GameOver(m_gameBoard)) {
+				m_currentPlayer = GetNextPlayer (m_currentPlayer);
+				if(m_currentPlayer == m_player2){
+					int computerMove = ComputeMove(m_gameBoard, m_currentPlayer, -1);
+					print (computerMove.ToString());
+					Button nextButton = GetButton(computerMove.ToString());
+					ChangeButton(nextButton);
+				}
+			}
 		}
 	}
 
-	private bool GameOver(){
+	private bool GameOver(int[] board){
 		bool gameOver = false;
-		if (CheckGameTied ()) {
+		if (CheckGameTied (board)) {
 			gameOver = true;
 			EndGameTied ();
-		} else if (CheckIfWon ()) {
+		} else if (CheckIfWon (board)) {
 			gameOver = true;
 			EndGameWon ();
 		}
@@ -71,8 +72,8 @@ public class TicTacToe : MonoBehaviour {
 
 	private bool UpdateGameBoardList(string spot){
 		int id = int.Parse (spot);
-		if (GetBoardSpot(id) == unused) {
-			SetBoardSpot(id, m_currentPlayer);
+		if (GetBoardSpot(id) == m_unused) {
+			SetBoardSpot(id, m_currentPlayer, m_gameBoard);
 			return true;
 		}
 		return false;
@@ -88,8 +89,8 @@ public class TicTacToe : MonoBehaviour {
 		return m_gameBoard [id];
 	}
 
-	private void SetBoardSpot(int id, int currentPlaye){
-		m_gameBoard [id] = currentPlaye;
+	private void SetBoardSpot(int id, int currentPlayer, int[] board){
+		board [id] = currentPlayer;
 	}
 
 	public string GetPlayerName(int playerId){
@@ -101,23 +102,27 @@ public class TicTacToe : MonoBehaviour {
 	}
 
 	public int GetNextPlayer(int currentPlayer){
-		if (currentPlayer == player1)	return player2;
-		else return player1;
+		if (currentPlayer == m_player1)	return m_player2;
+		else return m_player1;
+	}
+
+	public void SetPlayerUnset(){
+		m_currentPlayer = m_unused;
 	}
 
 	private int GetRandomPlayer(){
-		return Random.Range(player1,player2);
+		return Random.Range(m_player1,m_player2);
 	}
 
-	private bool TakenBySamePlayer(int a, int b, int c){
-		if (m_gameBoard [a] != unused && m_gameBoard [a] == m_gameBoard [b] && m_gameBoard [b] == m_gameBoard [c])
+	private bool TakenBySamePlayer(int[] board, int a, int b, int c){
+		if (m_gameBoard [a] != m_unused && m_gameBoard [a] == m_gameBoard [b] && m_gameBoard [b] == m_gameBoard [c])
 			return true;
 		return false;
 	}
 
-	private bool CheckIfWon(){
+	private bool CheckIfWon(int[] board){
 		for (int i = 0; i < m_winningCombinations.Length; i++) {
-			if(TakenBySamePlayer(m_winningCombinations[i][0], m_winningCombinations[i][1], m_winningCombinations[i][2])) {
+			if(TakenBySamePlayer(board, m_winningCombinations[i][0], m_winningCombinations[i][1], m_winningCombinations[i][2])) {
 				m_winningCombo = "Winning Combo: " + m_winningCombinations[i][0] + " " +  m_winningCombinations[i][1] + " " + m_winningCombinations[i][2];
 				return true;
 			}
@@ -125,10 +130,10 @@ public class TicTacToe : MonoBehaviour {
 		return false;
 	}
 
-	private bool CheckGameTied(){
-		int movesLeft = 9;
-		for(int i = 0; i < m_gameBoard.Length; i++){
-			if(m_gameBoard[i] != unused)movesLeft--;
+	private bool CheckGameTied(int[] board){
+		int movesLeft = board.Length;
+		for(int i = 0; i < board.Length; i++){
+			if(board[i] != m_unused)movesLeft--;
 		}
 
 		if(movesLeft>0) return false;
@@ -151,4 +156,59 @@ public class TicTacToe : MonoBehaviour {
 			print ("Position :" + i + " Player: " + m_gameBoard [i]);
 
 	}
+
+	public void ResetGame(){
+		SetPlayerUnset ();
+		InitializeBoard ();
+		ResetButtons ();
+		m_currentPlayer = GetRandomPlayer ();
+	}
+
+	private void ResetButtons(){
+		for(int i = 0; i < m_gameBoard.Length; i++){
+			Button bto = GetButton(i.ToString());
+			Text buttonText = bto.GetComponentInChildren<Text> () as Text;
+			buttonText.text = m_Players [m_currentPlayer] [1];
+		}
+	}
+
+	private Button GetButton(string buttonName){
+		return GameObject.Find(buttonName).GetComponent<Button>() as Button;
+	}
+
+	private int ComputeMove(int[] board, int currentPlayer, int previousMove){
+		int newMovePosition = -1;
+
+		if(CheckIfWon(board)) return previousMove;
+
+		int newWinner = -1;
+
+		for(int i = 0; i < board.Length; i++){
+			if(board[i] == m_unused){
+				SetBoardSpot(i, currentPlayer, board);
+				currentPlayer = GetNextPlayer(currentPlayer);
+				newMovePosition = ComputeMove(board, currentPlayer, m_unused);
+				SetBoardSpot(i, m_unused, board);
+
+				//print ("CurrenPlayer: " + currentPlayer + "  previousWinner: " + previousWinner + " newMovePosition: " + newMovePosition);
+
+				if((currentPlayer == m_player2 && previousMove == m_player1) || (currentPlayer == m_player1 && previousMove == m_player2)){
+					newWinner = currentPlayer;
+					newMovePosition = i;
+				}
+
+				if(newWinner == m_player2){
+					return newMovePosition;
+				}
+			}
+		}
+
+		if(newWinner == -1){
+			return newWinner;
+		}
+
+		return newMovePosition;
+	}
+
+
 }
